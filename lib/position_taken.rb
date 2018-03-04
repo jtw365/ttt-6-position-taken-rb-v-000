c59e2081c2da5b41a53f1c1e7849bc4cfe4cf1cb
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-04 01:45:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+# code your #position_taken? method her
+def position_taken?(board, index)
+  if board[index] == " "
+    puts "false"
+  elsif board[index] == ""
+    puts false
+  elsif board[index] == nil
+    puts false
+  else board[index] == "X" or "O"
+    puts true
+  end
+end
